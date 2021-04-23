65697edd6b8ca3df67150dbf8d00f7af894ae338
gerardnicolas <gerard.nicolas@benilde.edu.ph>
2021-04-24 05:25:14 +0800

    Added Working Contact Form with Email Notification

--- activity4/Controllers/HomeController.cs
+++ activity4/Controllers/HomeController.cs
@@ -7,6 +7,9 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 
+using System.Net;
+using System.Net.Mail;
+
 namespace activity4.Controllers
 {
     public class HomeController : Controller
@@ -41,6 +44,42 @@ namespace activity4.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Contact(Contact record)
+        {
+            using (MailMessage mail = new MailMessage("contact.virtualdoc@gmail.com", record.Email))
+            {
+                mail.Subject = record.Subject;
+
+                string message = "Hello, " + record.SenderName + "!<br/><br/>" +
+                    "Our team has received your inquiry. Here are the details: <br/><br/>" +
+                    "Contact Number: <strong>" + record.ContactNo + "</strong><br/>" +
+                    "Message:<br/><strong>" + record.Message + "</strong><br/><br/>" +
+                    "Please wait for our reply. Thank you!";
+                mail.Body = message;
+                mail.IsBodyHtml = true;
+
+                using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.EnableSsl = true;
+                    NetworkCredential NetworkCred =
+                        new NetworkCredential("contact.virtualdoc@gmail.com", "gboywanikissleged24");
+                    smtp.UseDefaultCredentials = true;
+                    smtp.Credentials = NetworkCred;
+                    smtp.Port = 587;
+                    smtp.Send(mail);
+                    ViewBag.Message = "Inquiry sent.";
+                }
+            }
+            return View();
         }
     }
 }
---
+++ activity4/Models/Contact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace activity4.Models
+{
+    public class Contact
+    {
+        [Display(Name = "Sender Name")]
+        [Required(ErrorMessage = "Required.")]
+        public string SenderName { get; set; }
+
+        [DataType(DataType.EmailAddress, ErrorMessage = "Invalid format.")]
+        [Required(ErrorMessage = "Required.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public string ContactNo { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "Required.")]
+        [DataType(DataType.MultilineText)]
+        public string Message { get; set; }
+    }
+}
